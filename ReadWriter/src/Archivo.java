8860b61f906033e9a85cd36af7d961f72a78e4c7
Luden20 <aachafla@puce.edu.ec>
2024-05-05 09:07:40 -0500

    h

---
+++ ReadWriter/src/Archivo.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Archivo {
+    private String Nombre;
+    private String Path;
+    private String Contenido;
+    
+    public Archivo (String Nombre, String Path, String Contenido){
+        this.Nombre = Nombre;
+        this.Path = Path;
+        this.Contenido = Contenido;
+    }
+    public String getNombre(){
+        return this.Nombre;
+    }
+    public String getPath(){
+        return this.Path;
+    }
+    public String getContenido(){
+        return this.Contenido;
+    }
+    public void guardarContenido() throws IOException {
+    File file = new File(Path, Nombre);
+    try (PrintWriter out = new PrintWriter(new FileWriter(file))) {
+            out.print(Contenido);
+        }
+    }
+}
---
+++ ReadWriter/src/Interfaz_read.java
@@ -0,0 +1,102 @@
+import java.io.*;
+public class Interfaz_read extends javax.swing.JFrame {
+
+    public Interfaz_read() {
+        initComponents();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jFileChooser1 = new javax.swing.JFileChooser();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 102, 102));
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT", 1, 24)); // NOI18N
+        jLabel1.setText("Leer un Archivo");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.insets = new java.awt.Insets(29, 29, 29, 29);
+        jPanel1.add(jLabel1, gridBagConstraints);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.PAGE_START);
+
+        jPanel2.setLayout(new java.awt.GridBagLayout());
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.insets = new java.awt.Insets(30, 30, 30, 30);
+        jPanel2.add(jScrollPane1, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        jPanel2.add(jFileChooser1, gridBagConstraints);
+
+        jButton1.setFont(new java.awt.Font("Tw Cen MT", 2, 24)); // NOI18N
+        jButton1.setText("Buscar un Archivo");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.insets = new java.awt.Insets(25, 25, 25, 25);
+        jPanel2.add(jButton1, gridBagConstraints);
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+         if (jFileChooser1.showOpenDialog(null) == jFileChooser1.APPROVE_OPTION) {
+                    File file = jFileChooser1.getSelectedFile();
+                    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            jTextArea1.append(line + "\n");
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    
+         }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interfaz_read().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JFileChooser jFileChooser1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    // End of variables declaration//GEN-END:variables
+}
